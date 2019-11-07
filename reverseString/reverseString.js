80861a295a6a5d434442f569a88bbbce5fb3c8a7
Nick Ford <fordn@fordn-W04.vmware.com>
2019-11-07 11:21:42 -0800

    reverse string solution

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+/* const reverseString = function() {
 
+}*/
+
+function reverseString (string) {
+    const splitString = string.split('')
+    const reverseString = splitString.reverse() 
+    const joinString = reverseString.join('')
+
+    return joinString
 }
+
 
 module.exports = reverseString
