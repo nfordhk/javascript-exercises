136d5a65620a81a69e705d217dc183da6719561a
Nick Ford <fordn@fordn-W04.vmware.com>
2019-11-08 16:12:06 -0800

    removeFromArray progress...

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -3,24 +3,33 @@
 *Type jasmine -v to check for it. If you need to install it, type npm install -g jasmine to do so.
 *To complete the exercise go to the exercise directory in a terminal and run jasmine filename.spec.js. 
 *This should find and run the test file and show you the output.
+*spec 1 - expect(removeFromArray([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+*spec 2 - expect(removeFromArray([1, 2, 3, 4], 3, 2)).toEqual([1, 4]);
 */
 
 /* const removeFromArray = function() {
 
 }*/
 
-function removeFromArray () {
-    //array for test
-    const numbers = [0, 1, 2, 3, 4]
+let array = [1, 2, 3, 4]
+let value = 3
 
-    //splice create a new array, assign to "remove"
-    remove = numbers.splice(2,1)
+function removeFromArray (array, value) {
+    //let array = [1, 2, 3, 4]
+    //let value = 3
+    let findValueIndex = array.indexOf(value)
+    array.splice(findValueIndex, 1)
 
-    //return array with removed index
-    return numbers
-    
+    return array
 }//end removeFromArray()
 
 console.log (removeFromArray())
-
 module.exports = removeFromArray
+
+/*function removeFromArray () {
+    
+    let array = [1, 2, 3, 4]
+    let splicedArray = array.splice(2,1)
+    return array
+
+}//end removeFromArray()*/ 
