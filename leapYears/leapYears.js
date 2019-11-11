290c2061001b6ceb2caea1cba02d1f61f0a5bfa0
Nick Ford <fordn@fordn-W04.vmware.com>
2019-11-11 13:29:51 -0800

    fixed bug in solution for leapYears

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -2,24 +2,26 @@
 //spec1 - expect(leapYears(1996)).toEqual(true);  -> % 4 ONLY 
 //spec2 - expect(leapYears(1997)).toEqual(false); -> NOTHING
 //spec3 - expect(leapYears(34992)).toEqual(true); -> % 4 ONLY
-//spec4 - expect(leapYears(1900)).toEqual(false); -> % 4 ONLY (FALSE), NOT % 400
+//spec4 - expect(leapYears(1900)).toEqual(false); -> % 4, % 100, NOT % 400
 //spec5 - expect(leapYears(1600)).toEqual(true);  -> % 4 AND % 400
-//spec6 - expect(leapYears(700)).toEqual(false);  -> % 4 ONLY (FALSE), NOT % 400
+//spec6 - expect(leapYears(700)).toEqual(false);  -> % 4, % 100, NOT % 400
 
 function leapYears(years) {
     if ((years % 4 === 0 && years % 400 === 0)) {
-        return "TRUE"
-    } else if (years % 4 === 0 && years % 400 !== 0) {
-        return "FALSE"
-    } else if (years % 4 !== 0) {
-        return "FALSE"
+        return true
+    } else if (years % 4 === 0 && years % 100 === 0 && years % 400 !== 0) {
+        return false
     } else if (years % 4 === 0) {
-        return "TRUE"
+        return true  
+    } else if (years % 4 !== 0) {
+        return false
     } else if (years % 400 === 0){
-        return "TRUE"
+        return true
     } 
 }
 
-console.log (leapYears(1900))
+console.log (leapYears(700))
 
 module.exports = leapYears
+
+
