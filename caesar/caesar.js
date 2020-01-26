39669d4446f26064f8424837ba8c5c03544e5677
Nick Ford <nfordhk@gmail.com>
2020-01-25 16:28:05 -0800

    caesar progress

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -10,12 +10,27 @@ Hint 3
 Leave anything that doesn’t come between A-Z as it is.
 */
 
-const caesar = function(string,indexShift) {
+/*const caesar = function(string,indexShift) {
     const asciiString = (string.charCodeAt(0)+indexShift)
+    //const asciiString = (string.charCodeAt(string.length)+indexShift)
+    console.log ('Console Log asciiString: ',asciiString)
     const cipherString = String.fromCharCode(asciiString)
+    console.log(cipherString)
     return cipherString
+}*/
+
+const caesar = function(string,indexShift) {
+    for (i = 0; i < string.length; i++){ //convert string to ascii characters
+        const splitString = string.split('')
+        console.log ('Console Log splieString:',splitString)
+        const asciiString = ((splitString.charCodeAt(i)+indexShift))
+        console.log ('Console Log asciiString:',asciiString)
+
+        //const cipherString = String.fromCharCode(asciiString) //convert ascii back to string after indexShift
+        //console.log ('Console Log cipherString:',cipherString)
+    }
 }
 
-caesar('A',1)
+caesar('Aaa',1)
 
 module.exports = caesar
